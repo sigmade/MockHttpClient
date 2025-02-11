6bee1aea606c46027156f9416ade9f8689ef6b51
Egor <sychov1991@mail.ru>
2025-02-11 15:35:41 +0500

    added test

--- TestProject1/MiddlewareTests.cs
+++ TestProject1/MiddlewareTests.cs
@@ -8,13 +8,15 @@ namespace UnitTests;
 public class MiddlewareTests
 {
     [Fact]
-    public async Task InvokeAsync_ExceptionThrown_ShouldLogErrorAndReturn500()
+    public async Task ShouldLogErrorAndReturn500()
     {
         var logger = Substitute.For<ILogger<ExceptionHandlingMiddleware>>();
         var exception = new InvalidOperationException("Test exception");
+        var wasExecuted = false;
 
         Task next(HttpContext context)
         {
+            wasExecuted = true;
             throw exception;
         }
 
@@ -23,11 +25,42 @@ public class MiddlewareTests
 
         await middleware.InvokeAsync(httpContext);
 
+        Assert.True(wasExecuted);
         Assert.Equal(StatusCodes.Status500InternalServerError, httpContext.Response.StatusCode);
 
         logger.Received(1).LogError(
             Arg.Any<EventId>(),
             exception,
+            "An error occurred while processing the request"
+        );
+    }
+
+
+
+    [Fact]
+    public async Task NoExceptionThrown_ShouldNotReturn500()
+    {
+        var logger = Substitute.For<ILogger<ExceptionHandlingMiddleware>>();
+        var wasExecuted = false;
+
+        Task next(HttpContext context)
+        {
+            wasExecuted = true;
+            context.Response.StatusCode = StatusCodes.Status200OK;
+            return Task.CompletedTask;
+        }
+
+        var middleware = new ExceptionHandlingMiddleware(next, logger);
+        var httpContext = new DefaultHttpContext();
+
+        await middleware.InvokeAsync(httpContext);
+
+        Assert.True(wasExecuted);
+        Assert.NotEqual(StatusCodes.Status500InternalServerError, httpContext.Response.StatusCode);
+
+        logger.DidNotReceive().LogError(
+            Arg.Any<EventId>(),
+            Arg.Any<Exception>(),
             "An error occurred while processing the request"
         );
     }
